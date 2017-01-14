83a44ff37b83797a3d626a28bdd94ce6be85e873
Gaurav Palande <gaurav.palande147@gmail.com>
2017-01-14 14:04:04 -0800

    Day 14 Commit after testing Logn efficiency algorithm

--- HackerRankTest/Program.cs
+++ HackerRankTest/Program.cs
@@ -1,35 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Linq;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    public Difference(int[] a)
+    {
+        elements = a;
+    }
+
+    public void computeDifference()
+    {
+        maximumDifference = maxDiff(elements)[1] - maxDiff(elements)[0];
+    }
+
+    public int[] maxDiff(int[] array)
+    {
+        int[] local = new int[2];
+        if (array.Length > 1)
+        {
+            int mid = array.Length / 2;
+            int[] leftLocal = maxDiff(array.Take(mid).ToArray());
+            int[] rightLocal = maxDiff(array.Skip(mid).ToArray());
+            local[0] = leftLocal[0] < rightLocal[0] ? leftLocal[0] : rightLocal[0];
+            local[1] = leftLocal[1] > rightLocal[1] ? leftLocal[1] : rightLocal[1];
+            return local;
+        }
+        else
+        {
+            return new int[] { array[0], array[0] };
+        }
+    }
+} // End of Difference Class
+
 class Solution
 {
-    static void Main(String[] args)
+    static void Main(string[] args)
     {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
-        int t = Convert.ToInt32(Console.ReadLine());
-        int n, root;
-        bool prime = true;
-        while (t-- > 0)
-        {
-            n = Convert.ToInt32(Console.ReadLine());
-            root = Convert.ToInt32(Math.Sqrt(n));
-            prime = true;
-            if (n == 1)
-                Console.WriteLine("Not prime");
-            else
-            {
-                while (root > 1)
-                {
-                    if (n % root == 0 && root != 1)
-                    {
-                        prime = false;
-                        break;
-                    }
-                    root--;
-                }
-                Console.WriteLine((prime ? "Prime" : "Not prime"));
-            }
-        }
+        Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+        Difference d = new Difference(a);
+
+        d.computeDifference();
+
+        Console.Write(d.maximumDifference);
         Console.ReadLine();
     }
 }
