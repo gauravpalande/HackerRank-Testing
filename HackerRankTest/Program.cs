fbbdd360179aff35ec22c165a0213c3772745745
Gaurav Palande <gaurav.palande147@gmail.com>
2017-01-16 19:13:19 -0800

    Solved String Reduction Algorithm

--- HackerRankTest/Program.cs
+++ HackerRankTest/Program.cs
@@ -1,53 +1,22 @@
 ﻿using System;
-using System.Linq;
-
-class Difference
-{
-    private int[] elements;
-    public int maximumDifference;
-
-    public Difference(int[] a)
-    {
-        elements = a;
-    }
-
-    public void computeDifference()
-    {
-        maximumDifference = maxDiff(elements)[1] - maxDiff(elements)[0];
-    }
-
-    public int[] maxDiff(int[] array)
-    {
-        int[] local = new int[2];
-        if (array.Length > 1)
-        {
-            int mid = array.Length / 2;
-            int[] leftLocal = maxDiff(array.Take(mid).ToArray());
-            int[] rightLocal = maxDiff(array.Skip(mid).ToArray());
-            local[0] = leftLocal[0] < rightLocal[0] ? leftLocal[0] : rightLocal[0];
-            local[1] = leftLocal[1] > rightLocal[1] ? leftLocal[1] : rightLocal[1];
-            return local;
-        }
-        else
-        {
-            return new int[] { array[0], array[0] };
-        }
-    }
-} // End of Difference Class
-
+using System.Collections.Generic;
+using System.IO;
 class Solution
 {
-    static void Main(string[] args)
+    static void Main(String[] args)
     {
-        Convert.ToInt32(Console.ReadLine());
-
-        int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
-
-        Difference d = new Difference(a);
-
-        d.computeDifference();
-
-        Console.Write(d.maximumDifference);
-        Console.ReadLine();
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        string input = Console.ReadLine();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (i != input.Length - 1)
+                if (input[i] == input[i + 1])
+                {
+                    input = input.Remove(i, 2);
+                    Console.WriteLine(input);
+                    i = -1;
+                }
+        }
+        Console.WriteLine(input == "" ? "Empty String" : input);
     }
 }
